f616ea6f28b14962068f14a60140029a420a5d3b
Michael Schlag <michaeldschlag@gmail.com>
2019-07-10 04:33:03 -0500

    step one complete templates made blogs working database working

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8888/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:cool@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'randomsecretkey'
 
+#mysql://root:build-a-blog@localhost/build-a-blog
+#mysql+pymysql://build-a-blog:cool@localhost:8889/build-a-blog
 
 class Blog(db.Model):
 
@@ -18,8 +21,11 @@ class Blog(db.Model):
         self.title = title
         self.content = content
 
+@app.route('/')
+def go():
+    return redirect("/blog")
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/blog', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
@@ -31,6 +37,23 @@ def index():
 
     blogs = Blog.query.all()
     return render_template('index.html',title="Build a Blog", blogs=blogs)
+
+@app.route('/new', methods=['POST', 'GET'])
+def new():
+
+    if request.method == 'POST':
+        blog_name = request.form['blog_name']
+        blog_content = request.form['blog_content']
+        if blog_name != "" and blog_content != "":
+            new_blog = Blog(blog_name, blog_content)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect('/blog')
+        else:
+            flash('Enter both fields', 'error')
+
+    blogs = Blog.query.all()
+    return render_template('new.html',title="Build a Blog", blogs=blogs)
 
 
 if __name__ == '__main__':
